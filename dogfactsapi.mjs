5639fa75e5d5031f787942ca5be140688ac1575a
Bennett Fife <33855430+bennettsf@users.noreply.github.com>
2024-02-07 03:18:30 -0800

    query string dog facts

---
+++ dogfactsapi.mjs
@@ -0,0 +1,35 @@
+import express from 'express'
+import {data} from './dog_facts.js'
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.get('/dogfacts', (req, res) => {
+    if ('number' in req.query) {
+        const num = req.query.number < data.length && req.query.number >= 0 ? req.query.number : -1
+        const facts = []
+
+        for (let i = 0; i < data.length; i++) {
+            facts.push(data[i])
+        }
+
+        if (num >= 0) {
+            res.send(facts)
+        } else {
+            res.send('Invalid Number Query')
+        }
+    } else {
+        res.send(data)
+    }
+    
+});
+
+app.use((req, res) => {
+    res.type('text/plain');
+    res.status(404);
+    res.send("404 - Not Found");
+})
+
+app.listen(app.get('port'), () => {
+    console.log("Express server is running!")
+});
