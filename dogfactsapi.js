45569a314e271524f7a99e48cef7e391cf9f73b3
Bennett Fife <33855430+bennettsf@users.noreply.github.com>
2024-02-07 03:32:21 -0800

    change file extension

--- dogfactsapi.js
+++ dogfactsapi.js
@@ -1,23 +1,24 @@
 import express from 'express'
-import {data} from './dog_facts.js'
+import { data } from './dog_facts.js'
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.get('/dogfacts', (req, res) => {
-    if ('number' in req.query) {
+    if ('number' in req.query && req.query.number < data.length) {
         const num = req.query.number < data.length && req.query.number >= 0 ? req.query.number : -1
         const facts = []
 
-        for (let i = 0; i < data.length; i++) {
-            facts.push(data[i])
-        }
+            for (let i = 0; i < num; i++) {
+                facts.push(data[i])
+            }
 
-        if (num >= 0) {
-            res.send(facts)
-        } else {
-            res.send('Invalid Number Query')
-        }
+            if (num >= 0) {
+                res.send(facts)
+            } else {
+                res.send('Invalid Number Query')
+            }
+            
     } else {
         res.send(data)
     }
